1f150a19013112a7a2130b34c785f3681a69813c
wiskassss234-sys <wiskassss234@gmail.com>
2026-03-21 14:25:00 -0600

    Crear propiedades.js

---
+++ propiedades.js
@@ -0,0 +1,25 @@
+const propiedades = [
+  {
+    nombre: "Terreno Matlapa",
+    precio: "$9,000,000 MXN",
+    ubicacion: "Matlapa, S.L.P.",
+    terreno: "2,163 m²",
+    extra: ["Servicios completos", "Frente a carretera"],
+    imagenes: ["matlapa1.jpg", "matlapa2.jpg"],
+    whatsapp: "Hola, quiero información sobre el terreno Matlapa"
+  },
+  {
+    nombre: "Bodega Tamuín",
+    precio: "$5,900,000 MXN",
+    ubicacion: "Tamuín, S.L.P.",
+    terreno: "1,600 m²",
+    construccion: "600 m²",
+    extra: ["Departamento 3 recámaras", "Todos los servicios"],
+    imagenes: [
+      "bodegatamuin1.jpg",
+      "bodegatamuin2.jpg",
+      "bodegatamuin3.jpg"
+    ],
+    whatsapp: "Hola, quiero información sobre la bodega Tamuín"
+  }
+];
